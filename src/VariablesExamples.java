d3a89e45413c496a6e2af9cb4a73904e63b7f55c
Kevin Gamboa Durán <kevin.gamboa@ulead.ac.cr>
2020-10-02 11:28:44 -0600

    Entrega Tarea 1

---
+++ src/VariablesExamples.java
@@ -0,0 +1,20 @@
+public class VariablesExamples {
+
+
+    public static void main(String args[]) {
+
+        //________________________________________________________//
+        short a = 32767; //Question#1: Direct Form
+        short a1 = Short.MAX_VALUE; //Question#1: Using Max value
+        //________________________________________________________//
+        long b = 2147483650L; //Question#2: Direct Form
+        long b1 = Integer.MAX_VALUE + 1L; //Question#2: Using Max Value
+        //________________________________________________________//
+        double c = 89.6; //Question#3
+
+
+        System.out.println(b1);
+
+
+    }
+}
